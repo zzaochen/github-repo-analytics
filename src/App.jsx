2d1352e37c1e909589e7532b34236a485d54c76e
zzaochen <zao@craftventures.com>
2026-02-18 14:51:18 -0800

    Revert "Add GitHub Repo Discovery feature"
    This reverts commit a20cd1d21781c0b83812dc5b9067e5321e72452f.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import TokenSettings from './components/TokenSettings';
 import CompareView from './components/CompareView';
 import TrendingView from './components/TrendingView';
 import MilestonesView from './components/MilestonesView';
-import RepoDiscovery from './components/RepoDiscovery';
 import HomePage from './components/HomePage';
 import BatchFetch from './components/BatchFetch';
 import RateLimitStatus from './components/RateLimitStatus';
@@ -816,16 +815,6 @@ function App() {
               >
                 Repo Milestones
               </Link>
-              <Link
-                to="/discovery"
-                className={`text-sm font-medium transition-colors ${
-                  location.pathname === '/discovery'
-                    ? 'text-blue-600'
-                    : 'text-gray-600 hover:text-gray-900'
-                }`}
-              >
-                Repo Discovery
-              </Link>
 
               {/* Auth Button */}
               {user ? (
@@ -915,9 +904,6 @@ function App() {
 
             {/* Milestones View */}
             <Route path="/milestones" element={<MilestonesView />} />
-
-            {/* Repo Discovery View */}
-            <Route path="/discovery" element={<RepoDiscovery />} />
           </Routes>
         </div>
       </div>
